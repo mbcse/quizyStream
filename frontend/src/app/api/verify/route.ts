44d70cc28e16dad7f63db64d7e556747ba8f0a09
mbcse <mbcse50@gmail.com>
2024-07-14 12:09:48 +0200

    Fix error

--- frontend/src/app/api/verify/route.ts
+++ frontend/src/app/api/verify/route.ts
@@ -1,5 +1,11 @@
 import { type IVerifyResponse, verifyCloudProof } from '@worldcoin/idkit'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+	const proof = req.body
+    const app_id = process.env.APP_ID
+    const action = process.env.ACTION_ID
+	const verifyRes = (await verifyCloudProof(proof, app_id, action)) as IVerifyResponse
 
+    return NextResponse.json(verifyRes, { status: 200 });
 };
